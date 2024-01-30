e6f5f9e7047fc4030b5b363fdc201482e3c63021
anush-hambardzumyan <foreveranham@gmail.com>
2024-01-30 19:17:52 +0400

    c

---
+++ types.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+std::vector<std::string> types = {"Bool","Char","Int","Float","Double","String"};
+std::map<std::string,int> intmap;
+std::map<std::string,double> doublemap;
+std::map<std::string,float> floatmap;
+std::map<std::string,bool> boolmap;
+std::map<std::string,char> charmap;
+std::map<std::string,std::string> stringmap;
+
+std::vector<std::pair<std::string,std::string>> allvars; //type and name
+std::vector<std::string> allkeywords
+{"Bool", "Char", "Int", "Float", "Double", "String", "if", "while", "std::cout<<", "std::cin>>" , "return", "break" , "continue"};
+
